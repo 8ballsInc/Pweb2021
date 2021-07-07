59baa2038fb67cceaa44e37adc0f5aea309e6fb7
Jiarre <38040300+Jiarre@users.noreply.github.com>
2021-07-07 12:42:18 +0200

    Minesweeper AJAX
    Progetto Minesweeper ma con sole richieste AJAX

---
+++ Minesweeper/src/java/it/disi/giarre/game/Cella.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.disi.giarre.game;
+
+/**
+ *
+ * @author giarr
+ */
+public class Cella {
+    int value;
+    boolean coperta;
+    
+    public Cella(){
+        this.value = 0;
+        this.coperta = true;
+    }
+}
